23195e4d0010c176b25b871536041c77b7008e60
Ai1Kira1Five <artemius16071998@gmail.com>
2015-08-05 20:28:18 +0300

    .

--- src/main/java/com/Arteman/HellLand/HellLand.java
+++ src/main/java/com/Arteman/HellLand/HellLand.java
@@ -51,6 +51,8 @@ public class HellLand {
     public static final int guiIDAmuletTable = 4;
     public static final int guiIDMMixer = 5;
 
+    public static final int guiIDBag = 6;
+
 
     @SidedProxy(clientSide = "com.Arteman.HellLand.proxy.ClientProxy", serverSide = "com.Arteman.HellLand.proxy.CommonProxy")
     public static CommonProxy artemanProxy;
