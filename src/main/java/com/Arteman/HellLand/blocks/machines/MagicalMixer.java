bf33946122fd6d704838815aa581919d644275d4
Ai1Kira1Five <artemius16071998@gmail.com>
2015-08-31 17:45:37 +0300

    MMixer

--- src/main/java/com/Arteman/HellLand/blocks/machines/MagicalMixer.java
+++ src/main/java/com/Arteman/HellLand/blocks/machines/MagicalMixer.java
@@ -1,58 +1,27 @@
 package com.Arteman.HellLand.blocks.machines;
 
 import com.Arteman.HellLand.HellLand;
-import com.Arteman.HellLand.ModBlocks;
 import com.Arteman.HellLand.tileentity.TileEntityMMixer;
-import com.Arteman.HellLand.utils.TEBlockHell;
-
 import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import java.util.Random;
 
-public class MagicalMixer extends TEBlockHell {
-	private final boolean isActive;
-
-    @SideOnly(Side.CLIENT)
-    private IIcon iconFront;
-
-    @SideOnly(Side.CLIENT)
-    private IIcon iconTop;
-
+public class MagicalMixer extends BlockContainer {
     private Random rand = new Random();
     private static final String __OBFID = "CL_00000207";
 
-    public MagicalMixer(String name, CreativeTabs creativeTabs, boolean isActive) {
-        super(name, creativeTabs);
-        this.isActive = false;
+    public MagicalMixer() {
+        super(Material.iron);
     }
 
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister iconRegister) {
-        this.blockIcon = iconRegister.registerIcon(HellLand.MODID + ":" + "mmSide");
-        this.iconFront = iconRegister.registerIcon(HellLand.MODID + ":" + (this.isActive ? "mmActive" : "mmIdle"));
-        this.iconTop = iconRegister.registerIcon(HellLand.MODID + ":" + "hellOvenTop");
-    }
-    
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int metadata) {
-        return side == 1 ? this.iconTop : (side == 1 ? this.iconTop : (side == 0 ? this.iconTop : (side != metadata ? this.blockIcon : this.iconFront)));
-    }
-    
     @Override
     public TileEntity createNewTileEntity(World world, int i) {
         return new TileEntityMMixer();
@@ -63,52 +32,5 @@ public class MagicalMixer extends TEBlockHell {
             FMLNetworkHandler.openGui(player, HellLand.instance, HellLand.guiIDMMixer, world, x, y, z);
         }
         return true;
-    }
-        
-    public void onBlockAdded(World world, int x, int y, int z){
-    	TileEntity tile = world.getTileEntity(x, y-1, z);
-    	if(tile instanceof TileEntityMMixer){
-    		
-    	}
-    }
-    
-    @SideOnly(Side.CLIENT)
-    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
-        if (this.isActive) {
-            int direction = world.getBlockMetadata(x, y, z);
-
-            float x1 = (float) x + 0.5F;
-            float y1 = (float) y + random.nextFloat();
-            float z1 = (float) z + 0.5F;
-
-            float f = 0.52F;
-            float f1 = random.nextFloat() * 0.6F - 0.3F;
-
-            if (direction == 4) {
-                world.spawnParticle("smoke", (double) x1 - f, (double) (y1), (double) (z1 + f1), 0D, 0D, 0D);
-                world.spawnParticle("flame", (double) x1 - f, (double) (y1), (double) (z1 + f1), 0D, 0D, 0D);
-            }
-
-            if (direction == 5) {
-                world.spawnParticle("smoke", (double) x1 + f, (double) (y1), (double) (z1 + f1), 0D, 0D, 0D);
-                world.spawnParticle("flame", (double) x1 + f, (double) (y1), (double) (z1 + f1), 0D, 0D, 0D);
-            }
-
-            if (direction == 2) {
-                world.spawnParticle("smoke", (double) x1 + f1, (double) (y1), (double) (z1 - f), 0D, 0D, 0D);
-                world.spawnParticle("flame", (double) x1 + f1, (double) (y1), (double) (z1 - f), 0D, 0D, 0D);
-            }
-
-            if (direction == 3) {
-                world.spawnParticle("smoke", (double) x1 + f1, (double) (y1), (double) (z1 + f), 0D, 0D, 0D);
-                world.spawnParticle("flame", (double) x1 + f1, (double) (y1), (double) (z1 + f), 0D, 0D, 0D);
-            }
-        }
-    }
-    
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entityplayer, ItemStack itemstack){
-    	if (itemstack.hasDisplayName()){
-    		((TileEntityMMixer) world.getTileEntity(x, y, z)).setGuiDisplayName(itemstack.getDisplayName());
-    	}
     }
 }
