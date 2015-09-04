ee74be9c36a4b45504e957f805f69e556f20b3ed
belovres <belovres@gmail.com>
2015-09-04 22:02:00 +0300

    Added interface

---
+++ src/main/java/com/Arteman/HellLand/utils/interfaces/IFacing.java
@@ -0,0 +1,8 @@
+package com.Arteman.HellLand.utils.interfaces;
+
+public interface IFacing {
+
+    public void setFacing(int facing);
+    public int getFacing();
+    public boolean isActive();
+}
